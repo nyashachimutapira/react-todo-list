f49c4ee87a5af4848a02751eb7c8a0ada8245c6c
nyashachimutapira <chimutapiran@gmail.com>
2026-01-02 18:12:04 +0200

    commit

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import React from 'react';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning>{children}</body>
+    </html>
+  );
+}
